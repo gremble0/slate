8d0c5ab031bb40342a7bc51bcbecdb84fc81b397
gremble0 <hermanstornes@gmail.com>
2023-11-27 05:28:03 +0100

    Moved SlateWindow class to separate file

---
+++ src/SlateWindow.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+
+#include "glibmm/refptr.h"
+#include "gtkmm/box.h"
+#include "gtkmm/enums.h"
+#include "gtkmm/scrolledwindow.h"
+#include "gtkmm/textbuffer.h"
+#include "gtkmm/textview.h"
+#include "ui.hpp"
+#include "SlateBuffer.hpp"
+#include "SlateWindow.hpp"
+
+// TODO: Slate::Window, Slate::Buffer, etc
+SlateWindow::SlateWindow()
+{
+    set_title("Slate");
+    set_child(view);
+    set_default_size(WINDOW_WIDTH, WINDOW_HEIGHT);
+    
+    // TODO: respect user config
+    scrolled_window.set_child(text_view);
+    scrolled_window.set_policy(Gtk::PolicyType::AUTOMATIC, Gtk::PolicyType::AUTOMATIC);
+    scrolled_window.set_expand();
+    view.append(scrolled_window);
+
+    SlateBuffer buffer = SlateBuffer("Welcome to Slate");
+    buffers.push_back(&buffer);
+    active_buffer = Gtk::TextBuffer::create();
+    SetActiveBuffer(buffer);
+}
+
+void SlateWindow::SetActiveBuffer(SlateBuffer &buffer)
+{
+    active_buffer->set_text(buffer.get_lines());
+    text_view.set_buffer(active_buffer);
+}
+
---
+++ src/SlateWindow.hpp
@@ -0,0 +1,19 @@
+#include "SlateBuffer.hpp"
+#include "gtkmm/box.h"
+#include "gtkmm/textview.h"
+#include "gtkmm/window.h"
+#include "gtkmm/scrolledwindow.h"
+
+class SlateWindow : public Gtk::Window 
+{
+public:
+    SlateWindow();
+    void SetActiveBuffer(SlateBuffer &buffer);
+protected:
+    // TODO: integrate our types with the GTK types
+    std::vector<SlateBuffer*> buffers;
+    Gtk::ScrolledWindow scrolled_window;
+    Gtk::TextView text_view;
+    Gtk::Box view;
+    Glib::RefPtr<Gtk::TextBuffer> active_buffer;
+};
