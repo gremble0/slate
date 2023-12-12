0254875e7d3f299efabe543000dd8d368fca157a
gremble0 <hermanstornes@gmail.com>
2023-12-12 02:58:41 +0100

    Fixes to lua mode api

--- api/mode.lua
+++ api/mode.lua
@@ -1,15 +1,16 @@
----Maps keybinds to a function and optionally a description
----@class KeyBind
----@field keys string
----@field command function
+---Maps keys to a function and optionally a description
+---@class Command
+---@field fun function
 ---@field description? string
 
+---Maps keys to commands
 ---@class Mode
----@field keymaps table<string, KeyBind[]>
+---@field name string
+---@field commands table<string, Command>
 
 ---Maps modes to an array of keybinds
 ---@class Modes
----@field modes table<string, Mode>
+---@field modes Mode[]
 local M = {}
 
 -- TODO: assign some default keybinds?
@@ -20,24 +21,40 @@ local M = {}
 ---@param command fun()
 ---@param desc? string
 M.set_keymap = function (mode, keys, command, desc)
-  local keybinds = M.modes[mode].keymaps[keys]
-  keybinds[#keybinds + 1] = {
-    keys = keys,
-    command = command,
-    description = desc
-  }
+  for _, m in pairs(M.modes) do
+    if m.name == mode then
+      m.commands[keys] = {
+        fun = command,
+        description = desc,
+      }
+    end
+  end
 end
 
 ---Removes a keymap from a mode, does not error if key is not mapped
 ---@param mode string
 ---@param keys string
 M.remove_keymap = function(mode, keys)
-  M.modes[mode].keymaps[keys] = nil
+  for _, m in pairs(M.modes) do
+    if m.name == mode then
+      m.commands[keys] = nil
+    end
+  end
 end
 
+---Adds a mode with no keybinds
 ---@param mode string name of new mode
 M.add_mode = function (mode)
-  M.modes[mode] = { keymaps = {} }
+  for _, m in pairs(M.modes) do
+    if m.name == mode then
+      error("Cannot create mode that already exists")
+    end
+  end
+
+  M.modes[#M.modes + 1] = {
+    name = mode,
+    commands = {},
+  }
 end
 
 return M {}
