fdf469ba326c762f3f7bf3e5839c5fe3cffc7d33
gremble0 <hermanstornes@gmail.com>
2023-12-13 20:52:10 +0100

    move compiled binary to bin folder, init sol state in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include "Window.hpp"
 
+#include <sol/sol.hpp>
 #include <gtkmm.h>
 
 using namespace Slate;
 
 int main(int argc, char **argv)
 { 
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+
     // Idk what the parameter means, from: https://gnome.pages.gitlab.gnome.org/gtkmm-documentation/chapter-basics.html
     auto app = Gtk::Application::create("org.gtkmm.slate");
     return app->make_window_and_run<Window>(argc, argv);
